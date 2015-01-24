2f756689b38de82a4ab260054a80b5635f6d2560
BobbyWatson <louis.arthur.cabrera@gmail.com>
2015-01-24 18:17:42 +0100

    begin of planete mixer

---
+++ Assets/Scripts/PlanetsMixer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlanetsMixer : MonoBehaviour {
+
+	[SerializeField]
+	float distanceByStep;
+	[SerializeField]
+	int totalSteps;
+	[SerializeField]
+	Transform[] planetes;
+
+	void OnEnable(){
+		GameManager.OnPlayerSet += SetPlanetes;
+	}
+
+	void SetPlanetes(){
+		planetes [0].position = new Vector3 (0, distanceByStep / 2 * totalSteps,0);
+		planetes [1].position = new Vector3 (0, -distanceByStep / 2 * totalSteps,0);
+	}
+}
