f7307ac2a9d82caaaec43c71a79481940170a28a
Herzucco <a.carta@student.isartdigital.com>
2015-01-24 05:30:57 +0100

    some network

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,8 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public enum Player{
+	ONE,
+	TWO
+}
 public class GameManager : BaseManager {
-
+	public static Player player;
+	
 	public static void Initialize(){
 
 	}
