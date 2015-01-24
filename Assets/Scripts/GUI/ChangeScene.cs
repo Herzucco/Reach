318b46ee3287ce91aca081795a7228169746bb03
BobbyWatson <louis.arthur.cabrera@gmail.com>
2015-01-24 12:58:20 +0100

    deconnexion page

---
+++ Assets/Scripts/GUI/ChangeScene.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeScene : MonoBehaviour {
+
+	public string sceneToLoad;
+
+	public void LoadScene(){
+		Application.LoadLevel (sceneToLoad);
+	}
+}
