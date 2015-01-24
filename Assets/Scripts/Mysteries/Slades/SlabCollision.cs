6fa4063e2cd07909489e687190eae5e24e33e6b9
Fontaine Thomas <tom_1993@hotmail.fr>
2015-01-24 05:24:14 +0100

    - Slab pattern & custom menu creation slab

---
+++ Assets/Scripts/Mysteries/Slades/SlabCollision.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlabCollision : Collision {
+
+    public bool isMandatory;
+    private bool _isActivated = false;
+
+    public bool isActivated
+    {
+        get { return _isActivated; }
+    }
+    protected override void PlayerEnter(GameObject player)
+    {
+
+        if (_isActivated)
+            _isActivated = false;
+        else
+            _isActivated = true;
+    }
+}
