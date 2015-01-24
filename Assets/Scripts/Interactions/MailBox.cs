1f107bb3e9a990b134cc4d045219d15a9d522166
Herzucco <a.carta@student.isartdigital.com>
2015-01-24 09:57:54 +0100

    commit before push

---
+++ Assets/Scripts/Interactions/MailBox.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof (PhotonView))]
+public class MailBox : ContextAction {
+	
+	public Renderer renderer;
+	private PhotonView photonView;
+	
+	public override void Action ()
+	{
+		renderer.material.color = new Color (Random.Range (0f, 1f), Random.Range (0f, 1f), Random.Range (0f, 1f));
+	}
+}
