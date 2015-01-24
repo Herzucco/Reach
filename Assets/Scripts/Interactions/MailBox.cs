1f30b6003b0bff45cf012df759568172c88bbacf
Herzucco <a.carta@student.isartdigital.com>
2015-01-24 10:18:11 +0100

    commit before merge

--- Assets/Scripts/Interactions/MailBox.cs
+++ Assets/Scripts/Interactions/MailBox.cs
@@ -3,12 +3,42 @@ using System.Collections;
 
 [RequireComponent(typeof (PhotonView))]
 public class MailBox : ContextAction {
-	
+	public Transform oneSpawn;
+	public Transform twoSpawn;
 	public Renderer renderer;
+	public bool isFull = false;
+
 	private PhotonView photonView;
-	
+
+	protected virtual void Start(){
+		photonView = GetComponent<PhotonView> ();
+		GameManager.OnPlayerSet += OnGameReady;
+
+	}
+
 	public override void Action ()
 	{
-		renderer.material.color = new Color (Random.Range (0f, 1f), Random.Range (0f, 1f), Random.Range (0f, 1f));
+		TriggerMailBox ();
+		if (isFull) {
+
+		} else {
+
+		}
+	}
+
+	[RPC]
+	protected virtual void ListenMailBox(){
+	}
+	
+	public virtual void TriggerMailBox(){
+		photonView.RPC("ListenMailBox", PhotonTargets.All);
+	}
+		
+	void OnGameReady(){
+		if (GameManager.player == Player.ONE) {
+			transform.position = oneSpawn.position;
+		} else {
+			transform.position = twoSpawn.position;
+		}
 	}
 }
