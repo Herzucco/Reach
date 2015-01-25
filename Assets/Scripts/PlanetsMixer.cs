c53d752d60204402fa6ab38d62c34035d4a541ca
BobbyWatson <louis.arthur.cabrera@gmail.com>
2015-01-25 15:42:02 +0100

    fix bug retry

--- Assets/Scripts/PlanetsMixer.cs
+++ Assets/Scripts/PlanetsMixer.cs
@@ -28,6 +28,10 @@ public class PlanetsMixer : MonoBehaviour {
 		GameManager.OnPlayerSet += SetPlanetes;
 	}
 
+	void Disable(){
+		GameManager.OnPlayerSet -= SetPlanetes;
+	}
+
 	void SetPlanetes(){
 		planetes [0].position = new Vector3 (0, distanceByStep / 2 * totalSteps,0);
 		planetes [1].position = new Vector3 (0, -distanceByStep / 2 * totalSteps,0);
