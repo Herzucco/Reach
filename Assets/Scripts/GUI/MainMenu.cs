d2b542e86a29bbaada17deef744e850948ec1159
Grégoire Carabeufs <Gregoire.Carabeufs@gmail.com>
2015-01-25 03:58:52 +0100

    Main Menu mieux

---
+++ Assets/Scripts/GUI/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	public string LevelToLoad;
+	public GameObject MainMenuContainer;
+	public GameObject CreditsMenuContainer;
+
+	public void goToGame()
+	{
+		Application.LoadLevel(LevelToLoad);
+	}
+
+	public void goToCredits()
+	{
+		CreditsMenuContainer.SetActive(true);
+		MainMenuContainer.SetActive(false);
+	}
+
+	public void goToMainMenu()
+	{
+		MainMenuContainer.SetActive(true);
+		CreditsMenuContainer.SetActive(false);
+	}
+}
