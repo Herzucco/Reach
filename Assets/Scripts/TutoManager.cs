16fdbaaad1f0317991de203480acf4dd8ae6c7fa
Grégoire Carabeufs <Gregoire.Carabeufs@gmail.com>
2015-01-25 08:02:16 +0100

    Tuto manager

---
+++ Assets/Scripts/TutoManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutoManager : MonoBehaviour {
+	public float animDuration;
+	public GameObject[] allLabels;
+	private int i;
+	// Use this for initialization
+	void Start () {
+		StartCoroutine("ChangeAnim");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (i == allLabels.Length)
+		{
+			StopCoroutine("ChangeAnim");
+		}
+	}
+
+	IEnumerator ChangeAnim(){
+		for(;;){
+			yield return new WaitForSeconds(animDuration);
+			allLabels[i].animation.Play();
+			i += 1;
+		}
+	}
+}
