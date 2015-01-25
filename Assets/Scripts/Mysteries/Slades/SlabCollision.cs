2b6951951c42d2c4f72dce681ba630334e477a62
Fontaine Thomas <tom_1993@hotmail.fr>
2015-01-25 11:03:29 +0100

    - Pushing slabs enigma

--- Assets/Scripts/Mysteries/Slades/SlabCollision.cs
+++ Assets/Scripts/Mysteries/Slades/SlabCollision.cs
@@ -4,17 +4,23 @@ using System.Collections;
 public class SlabCollision : Collision {
 
     public bool isMandatory;
+    public Color color = Color.white;
     private bool _isActivated = false;
 
     public bool isActivated
     {
         get { return _isActivated; }
     }
+
+    private void Start()
+    {
+        renderer.material.color = color;
+    }
     protected override void PlayerEnter(GameObject player)
     {
         if (_isActivated)
         {
-            renderer.material.color = Color.red;
+            renderer.material.color = Color.white;
             _isActivated = false;
         }
         else
