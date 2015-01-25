30970a583cd69367dcf6575ba4980e16dac60bc2
Herzucco <a.carta@student.isartdigital.com>
2015-01-25 13:50:03 +0100

    no message

--- Assets/Scripts/Interactions/MailBox.cs
+++ Assets/Scripts/Interactions/MailBox.cs
@@ -16,8 +16,6 @@ public class MailBox : MonoBehaviour {
 	public AudioClip inboxSound;
 	public AudioClip outboxSound;
 	protected AudioSource soundSource;
-	private int times = 0;
-	private byte[] b;
 
 	protected virtual void Start(){
 		photonView = GetComponent<PhotonView> ();
@@ -26,29 +24,16 @@ public class MailBox : MonoBehaviour {
 	}
 
 	[RPC]
-	protected virtual void ListenMailBox(byte[] receivedByte){
-		times++;
-		if (times >= 2) {
-			byte[] c = new byte[receivedByte.Length*2];
-			for(int i = 0; i < c.Length; i++){
-				if(i < receivedByte.Length){
-					c[i] = b[i];
-				}else{
-					c[i] = receivedByte[i];
-				}
-			}
-			receivedTexture = new Texture2D(1, 1);
-			receivedTexture.LoadImage(c);
-			isFull = true;
-			snap = Camera.main.gameObject.GetComponent<PlayerSnap> ();
-			snap.SetTexture(receivedTexture);
-			
-			soundSource.clip = outboxSound;
-			soundSource.Play();
-		}else{
-			b = receivedByte;
-		}
+	protected virtual void ListenMailBox(Color[] receivedByte){
+		Debug.Log ("oker");
+		receivedTexture = new Texture2D(1, 1);
+		receivedTexture.SetPixels(receivedByte);
+		isFull = true;
+		snap = Camera.main.gameObject.GetComponent<PlayerSnap> ();
+		snap.SetTexture(receivedTexture);
 
+		soundSource.clip = outboxSound;
+		soundSource.Play();
 	}
 	
 	public virtual void TriggerMailBox(){
@@ -56,20 +41,7 @@ public class MailBox : MonoBehaviour {
 		if (snap.TextureToSend != null) {
 			Debug.Log("RPC");
 			Debug.Log(snap.TextureToSend.EncodeToPNG().Length);
-			byte[] grotruk = snap.TextureToSend.EncodeToPNG();
-			byte[] petitbidule = new byte[grotruk.Length/2];
-			byte[] petitbidule2 = new byte[grotruk.Length/2];
-			for(int i = 0; i < grotruk.Length/2; i++){
-				petitbidule[i] = grotruk[i];
-			} 
-
-			for(int i = grotruk.Length/2 - 1; i < grotruk.Length; i++){
-				petitbidule2[i] = grotruk[i];
-			} 
-
-			Debug.Log(petitbidule.Length);
-			photonView.RPC("ListenMailBox", PhotonTargets.Others, petitbidule);
-			photonView.RPC("ListenMailBox", PhotonTargets.Others, petitbidule2);
+			photonView.RPC("ListenMailBox", PhotonTargets.Others, snap.TextureToSend.GetPixels());
 		}
 	}
 		
