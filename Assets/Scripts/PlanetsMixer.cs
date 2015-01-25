b803515ae2d4da7577d919003baf2aebb55fde3a
BobbyWatson <louis.arthur.cabrera@gmail.com>
2015-01-25 12:58:04 +0100

    log

--- Assets/Scripts/PlanetsMixer.cs
+++ Assets/Scripts/PlanetsMixer.cs
@@ -31,6 +31,7 @@ public class PlanetsMixer : MonoBehaviour {
 	void SetPlanetes(){
 		planetes [0].position = new Vector3 (0, distanceByStep / 2 * totalSteps,0);
 		planetes [1].position = new Vector3 (0, -distanceByStep / 2 * totalSteps,0);
+		Debug.Log (planetes [0].position);
 	}
 
 	void Update(){
